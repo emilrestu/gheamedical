1dffc07d73706f87e1a2ee88c1f09c2a37ce2a07
Famil Restu Pambudi <famil.restu@dataon.com>
2024-06-21 21:45:29 +0700

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+function App() {
+    return (
+        <>
+            <div className="flex justify-center align-middle">
+                <div className="w-12 h-12 bg-slate-500" />
+            </div>
+            <div className="flex justify-center items-center h-screen">
+                <h1 className="text-4xl font-bold text-blue-500">Hello, Vite + React + Tailwind CSS!</h1>
+            </div>
+        </>
+    );
+}
+
+export default App;
