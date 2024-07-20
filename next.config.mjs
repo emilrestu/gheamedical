7dbed082cee49016db7d3384ee4e1ef402fd34b9
Famil Restu Pambudi <famil.restu@dataon.com>
2024-07-20 08:44:22 +0700

    fix: for cpanel

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
+    output: 'export',
     reactStrictMode: true,
     transpilePackages: ['antd', '@ant-design/icons', '@ant-design/icons-svg', 'rc-util', 'rc-pagination', 'rc-picker', 'rc-tree', 'rc-table'],
-    // images: { unoptimized: true },
+    images: { unoptimized: true },
 };
 
 export default nextConfig;
