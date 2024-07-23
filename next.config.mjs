01e3d0e33475634376c262ad88ca6b4865d6180b
Famil Restu Pambudi <famil.restu@dataon.com>
2024-07-23 22:32:08 +0700

    feat: add import svg

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ const nextConfig = {
     reactStrictMode: true,
     transpilePackages: ['antd', '@ant-design/icons', '@ant-design/icons-svg', 'rc-util', 'rc-pagination', 'rc-picker', 'rc-tree', 'rc-table'],
     images: { unoptimized: true },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
 };
 
 export default nextConfig;
