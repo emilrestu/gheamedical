a6a57827d682a9e8453e00b9496b671fa1db1780
Famil Restu Pambudi <famil.restu@dataon.com>
2024-07-27 17:30:11 +0700

    fix: company logo

--- src/components/company-logo/CompanyLogo.tsx
+++ src/components/company-logo/CompanyLogo.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import CompanyLogoBig from '../../assets/logo/logo_01_01.png';
+import CompanyLogoBig from '@/assets/logo/logo_01_01.png';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const CompanyLogo = () => {
     return (
-        <Link href="/">
+        <Link href="/" className="company-logo">
             <Image priority height={32} src={CompanyLogoBig} alt="GHEA services" />
         </Link>
     );
