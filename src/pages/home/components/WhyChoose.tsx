3e813faae6839b34154c4fcb3fcc7120cbf86db7
Famil Restu Pambudi <famil.restu@dataon.com>
2024-07-27 16:11:23 +0700

    feat: why choose images

--- src/pages/home/components/WhyChoose.tsx
+++ src/pages/home/components/WhyChoose.tsx
@@ -3,50 +3,35 @@ import Translate from '@/components/translate';
 import { SECONDARY_COLOR } from '@/data/constants';
 import { Card, Carousel, Col, Row, theme, Typography } from 'antd';
 import { CarouselRef } from 'antd/es/carousel';
-import React, { useRef, useState } from 'react';
-
-type ChooseData = {
-    title: string;
-    icon: 'badge' | 'wellness' | 'doctor';
-    description: string;
-    image: string;
-    alt: string;
-}[];
-
-const WhyChooseData: ChooseData = [
-    {
-        title: 'Superior Quality Assistant',
-        icon: 'badge',
-        description:
-            "GHEA Services offers exceptional quality assistance and support tailored to meet your family' health and personal care needs, ensuring you receive the best possible service.",
-        image: '',
-        alt: 'superior-quality-assistant',
-    },
-    {
-        title: 'Personalized Wellness Care',
-        icon: 'wellness',
-        description:
-            'Our comprehensive approach ensures that every aspect of your well-being is addressed, providing you and your family with exceptional care and support..',
-        image: '',
-        alt: 'personalized-wellness-care',
-    },
-    {
-        title: 'Services with Best Individuals',
-        icon: 'doctor',
-        description:
-            'We have assembled a team of highly skilled and compassionate professionals dedicated to providing the highest quality care. Our team brings expertise, experience, and a genuine commitment to your well-being.',
-        image: '',
-        alt: 'services-with-best-individuals',
-    },
-];
+import React, { useMemo, useRef, useState } from 'react';
+import WhyChooseImage01 from '../assets/why-choose-01.jpeg';
+import WhyChooseImage02 from '../assets/banner-03.jpeg';
+import WhyChooseImage03 from '../assets/banner-04.jpeg';
+import WhyChooseEnglish from '@/data/why-choose/en.json';
+import WhyChooseArabic from '@/data/why-choose/ar.json';
+import { useTranslateContext } from '@/context/TranslateContext';
+import { StaticImageData } from 'next/image';
 
 const WhyChoose: React.FC = () => {
     const {
         token: { colorPrimary },
     } = theme.useToken();
+    const { langCode } = useTranslateContext();
 
     const [activeSlide, setActiveSlide] = useState(0);
     const sliderRef = useRef<CarouselRef>(null);
+
+    const WhyChooseData = useMemo(() => {
+        if (langCode === 'ar') return WhyChooseArabic;
+
+        return WhyChooseEnglish;
+    }, [langCode]);
+
+    const WhyChooseImage: { [key: string]: StaticImageData } = {
+        whychoose01: WhyChooseImage01,
+        whychoose02: WhyChooseImage02,
+        whychoose03: WhyChooseImage03,
+    };
 
     return (
         <Row className="home-content-wrapper why-choose-container" gutter={[16, 16]}>
@@ -71,13 +56,12 @@ const WhyChoose: React.FC = () => {
                                 {...(activeSlide === i && { style: { backgroundColor: colorPrimary } })}
                                 onClick={() => {
                                     sliderRef?.current?.goTo(i);
-                                    // setActiveSlide(i);
                                 }}
                             >
                                 <Row gutter={[16, 16]}>
                                     <Col span={24} className="title">
                                         <div className="icon-container" {...(activeSlide === i && { style: { backgroundColor: SECONDARY_COLOR } })}>
-                                            <SVGIcon icon={item.icon} />
+                                            <SVGIcon icon={item.icon as 'badge' | 'wellness' | 'doctor'} />
                                         </div>
                                         <Typography.Text>{item.title}</Typography.Text>
                                     </Col>
@@ -106,7 +90,10 @@ const WhyChoose: React.FC = () => {
                 >
                     {WhyChooseData.map((item, i) => (
                         <div key={i}>
-                            <Card style={{ background: colorPrimary, flex: 1, height: 687 }}>{item.title}</Card>
+                            <Card style={{ flex: 1, height: 687 }}>
+                                <div className="why-choose-image" style={{ backgroundImage: `url(${WhyChooseImage[item.image].src})` }} />
+                                <div className="why-choose-background-mask" />
+                            </Card>
                         </div>
                     ))}
                 </Carousel>
