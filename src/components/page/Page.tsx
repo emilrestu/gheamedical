0d815143a66f81e9b891ec406f8ea3234d1d0399
Famil Restu Pambudi <famil.restu@dataon.com>
2024-08-17 21:15:20 +0700

    fix: banner home on smaller screen

--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -1,16 +1,15 @@
 import React, { CSSProperties } from 'react';
 import MetaHead from '../meta-head';
-import { Grid } from 'antd';
 
 type PageType = { pageTitle?: string; style?: CSSProperties; children: React.ReactNode };
 
 const Page: React.FC<PageType> = ({ pageTitle, style, children }) => {
-    const { xs } = Grid.useBreakpoint();
-
     return (
         <>
             <MetaHead pageTitle={pageTitle} />
-            <div style={{ paddingInline: xs ? 16 : 120, ...style }}>{children}</div>
+            <div className="page" style={{ ...style }}>
+                {children}
+            </div>
         </>
     );
 };
