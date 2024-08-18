282d230643889caf31a51897cbc10bfb1c103e2d
Famil Restu Pambudi <famil.restu@dataon.com>
2024-08-18 20:05:22 +0700

    chore: test deploy vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    // output: 'export',
     reactStrictMode: true,
     transpilePackages: ['antd', '@ant-design/icons', '@ant-design/icons-svg', 'rc-util', 'rc-pagination', 'rc-picker', 'rc-tree', 'rc-table'],
     images: { unoptimized: true },
