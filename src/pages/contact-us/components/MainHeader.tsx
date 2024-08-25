67e8649c20deb0d5267c056d25f566970e5e1efb
Famil Restu Pambudi <famil.restu@dataon.com>
2024-08-25 18:58:11 +0700

    feat: update contact us

--- src/pages/contact-us/components/MainHeader.tsx
+++ src/pages/contact-us/components/MainHeader.tsx
@@ -5,6 +5,7 @@ import SVGIcon from '@/components/svg-icon';
 import Link from 'next/link';
 import { PHONE_NUMBER } from '@/data/constants';
 import { ClockCircleOutlined } from '@ant-design/icons';
+import BookServiceCard from '@/components/book-service-card';
 
 const MainHeader = () => {
     return (
@@ -18,73 +19,116 @@ const MainHeader = () => {
                     </div>
                 </Col>
             </Row>
-            <Row className="contact-us-details">
-                <Col md={24} className="details-wrapper">
-                    <Card>
-                        <div className="icons-container">
-                            <div className="icons">
-                                <SVGIcon icon="phone" />
-                            </div>
-                        </div>
-                        <Typography.Text className="title">
-                            <Translate>Phone</Translate>
-                        </Typography.Text>
-                        <Typography.Text className="description">
-                            <Link href={`tel:${PHONE_NUMBER}`} passHref>
-                                <Translate>+966 54 114 2624</Translate>
-                            </Link>
-                        </Typography.Text>
-                    </Card>
 
-                    <Card>
-                        <div className="icons-container">
-                            <div className="icons">
-                                <SVGIcon icon="mail" />
-                            </div>
-                        </div>
+            <Row className="contact-us" gutter={[16, 16]}>
+                <Col sm={24} md={14}>
+                    <Card style={{ width: '100%' }}>
                         <Typography.Text className="title">
-                            <Translate>Email</Translate>
+                            <Translate>Contact Info</Translate>
                         </Typography.Text>
-                        <Typography.Text className="description">
-                            <Link href={`mailto:info@gheamedical.com`} passHref>
-                                <Translate>info@gheamedical.com</Translate>
-                            </Link>
-                        </Typography.Text>
-                    </Card>
 
-                    <Card>
-                        <div className="icons-container">
-                            <div className="icons">
-                                <ClockCircleOutlined width={32} height={32} style={{ fontSize: 28 }} />
-                            </div>
-                        </div>
-                        <Typography.Text className="title">
-                            <Translate>Work Hours</Translate>
-                        </Typography.Text>
-                        <Typography.Text className="description">
-                            <Translate>08:00 - 17:00</Translate>
-                        </Typography.Text>
+                        <Row>
+                            <Col sm={24}>
+                                <div className="icons-container">
+                                    <div className="icons">
+                                        <SVGIcon icon="phone" />
+                                    </div>
+
+                                    <Row style={{ flex: 'inherit' }}>
+                                        <Col sm={24}>
+                                            <Typography.Text className="title">
+                                                <Translate>Phone</Translate>
+                                            </Typography.Text>
+                                        </Col>
+
+                                        <Col sm={24}>
+                                            <Typography.Text className="description">
+                                                <Link href={`tel:${PHONE_NUMBER}`} passHref>
+                                                    <Translate>+966 54 114 2624</Translate>
+                                                </Link>
+                                            </Typography.Text>
+                                        </Col>
+                                    </Row>
+                                </div>
+                            </Col>
+
+                            <Col sm={24}>
+                                <div className="icons-container">
+                                    <div className="icons">
+                                        <SVGIcon icon="mail" />
+                                    </div>
+
+                                    <Row style={{ flex: 'inherit' }}>
+                                        <Col sm={24}>
+                                            <Typography.Text className="title">
+                                                <Translate>Email</Translate>
+                                            </Typography.Text>
+                                        </Col>
+
+                                        <Col sm={24}>
+                                            <Typography.Text className="description">
+                                                <Link href={`mailto:info@gheamedical.com`} passHref>
+                                                    <Translate>info@gheamedical.com</Translate>
+                                                </Link>
+                                            </Typography.Text>
+                                        </Col>
+                                    </Row>
+                                </div>
+                            </Col>
+
+                            <Col sm={24}>
+                                <div className="icons-container">
+                                    <div className="icons">
+                                        <ClockCircleOutlined width={32} height={32} style={{ fontSize: 28 }} />
+                                    </div>
+
+                                    <Row style={{ flex: 'inherit' }}>
+                                        <Col sm={24}>
+                                            <Typography.Text className="title">
+                                                <Translate>Work Hours</Translate>
+                                            </Typography.Text>
+                                        </Col>
+                                        <Col sm={24}>
+                                            <Typography.Text className="description">
+                                                <Translate>08:00 - 17:00</Translate>
+                                            </Typography.Text>
+                                        </Col>
+                                    </Row>
+                                </div>
+                            </Col>
+
+                            <Col sm={24}>
+                                <div className="icons-container">
+                                    <div className="icons">
+                                        <SVGIcon icon="location" />
+                                    </div>
+
+                                    <Row style={{ flex: 'inherit' }}>
+                                        <Col sm={24}>
+                                            <Typography.Text className="title">
+                                                <Translate>Address</Translate>
+                                            </Typography.Text>
+                                        </Col>
+
+                                        <Col sm={24}>
+                                            <Typography.Text className="description">
+                                                <Link href="https://maps.app.goo.gl/YYDMibRzYyfDC6JH8" passHref target="_blank">
+                                                    <Translate>
+                                                        بيسكس مساحات عمل مشتركة Basix Coworking Space No 2, 7755 Ahmad Al Attas, Al Zahra, Jeddah
+                                                        23425, Saudi Arabia
+                                                    </Translate>
+                                                </Link>
+                                            </Typography.Text>
+                                        </Col>
+                                    </Row>
+                                </div>
+                            </Col>
+                        </Row>
                     </Card>
                 </Col>
 
-                <Col md={24} style={{ marginTop: 16 }} className="address-wrapper">
-                    <Card>
-                        <div className="icons-container">
-                            <div className="icons">
-                                <SVGIcon icon="location" />
-                            </div>
-                        </div>
-                        <Typography.Text className="title">
-                            <Translate>Address</Translate>
-                        </Typography.Text>
-                        <Typography.Text className="description">
-                            <Link href="https://maps.app.goo.gl/YYDMibRzYyfDC6JH8" passHref target="_blank">
-                                <Translate>
-                                    بيسكس مساحات عمل مشتركة Basix Coworking Space No 2, 7755 Ahmad Al Attas, Al Zahra, Jeddah 23425, Saudi Arabia
-                                </Translate>
-                            </Link>
-                        </Typography.Text>
-                    </Card>
+                <Col sm={24} md={10}>
+                    <BookServiceCard />
                 </Col>
             </Row>
         </>
