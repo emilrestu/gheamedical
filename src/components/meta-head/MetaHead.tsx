c7d8301bd1635afad02f9828eda67f9202de941b
Famil Restu Pambudi <famil.restu@dataon.com>
2024-08-28 22:28:34 +0700

    feat: update keywords

--- src/components/meta-head/MetaHead.tsx
+++ src/components/meta-head/MetaHead.tsx
@@ -11,7 +11,7 @@ const MetaHead: React.FC<MetaHeadType> = ({ pageTitle = 'Ghea Medical' }) => {
             <title>{pageTitle}</title>
             <meta name="viewport" content="width=device-width,shrink-to-fit=no,initial-scale=1,maximum-scale=1,user-scalable=0" />
             <meta name="description" content="خدمات استشارات الرعاية الصحية بالمملكة العربية السعودية" />
-            <meta name="keywords" content="ابشر افراد" />
+            <meta name="keywords" content="ممرضة منزلية جدة,ابشر افراد" />
             <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         </Head>
     );
